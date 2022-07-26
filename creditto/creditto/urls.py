93d606c98ec3aa41c2ff5ddaff624cc3cdd91ea7
Max Tuchin <maxt86@pm.me>
2022-07-26 22:49:57 +0300

    Change admin panel location for security concerns

--- creditto/creditto/urls.py
+++ creditto/creditto/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+from django.contrib import admin as oneandonlygod
 
 from django.urls import include
 from django.urls import path
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('oneandonlygod/', oneandonlygod.site.urls),
     path('', include('landing.urls')),
 ]
