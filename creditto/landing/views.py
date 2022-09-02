1362b7c1aace49b17759553059de92f24e196ede
Max Tuchin <maxt86@pm.me>
2022-09-02 03:45:14 +0300

    Add redirect to posts page for authenticated users in index view

--- creditto/landing/views.py
+++ creditto/landing/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+
 from django.views import View
 
 
 class IndexView(View):
     
     def get(self, request, *args, **kwargs):
-        return render(request, 'landing/index.html')
+        return (
+            render(request, 'landing/index.html'),
+            redirect('posts'),
+        )[request.user.is_authenticated]
